30731c1917374ac4890498dd7b03a0cca0c1ec4b
Sven Otto <svenotto@uw.edu>
2020-06-03 19:08:38 -0700

    Updates

---
+++ precache-manifest.3efaed3ceae16c1f5aff2ebbe45d038a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8f2317d4e680ecd5c60f714010546c2",
+    "url": "/nyt-reading-list/index.html"
+  },
+  {
+    "revision": "48a80a04f4b65cd6f0c4",
+    "url": "/nyt-reading-list/static/css/main.7be64aad.chunk.css"
+  },
+  {
+    "revision": "4b72625fda5672911f28",
+    "url": "/nyt-reading-list/static/js/2.78217677.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nyt-reading-list/static/js/2.78217677.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48a80a04f4b65cd6f0c4",
+    "url": "/nyt-reading-list/static/js/main.380540f0.chunk.js"
+  },
+  {
+    "revision": "d4c55352ce5d5dc7445a",
+    "url": "/nyt-reading-list/static/js/runtime-main.f4733277.js"
+  },
+  {
+    "revision": "ffdb5b1a456f0d16da4a75e1a365d749",
+    "url": "/nyt-reading-list/static/media/book.ffdb5b1a.svg"
+  },
+  {
+    "revision": "3aecda0cd334c83cebe7032dc5108b75",
+    "url": "/nyt-reading-list/static/media/empire-state.3aecda0c.svg"
+  }
+]);
